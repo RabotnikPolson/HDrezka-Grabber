e630d94a6f305caf839ea59555f73f9f645d4ada
Damir Abdikadirov <ghostgames077@gmail.com>
2026-04-23 18:56:26 +0500

    Delete src/components directory

--- src/components/Layout.tsx
+++
@@ -1,7 +0,0 @@
-export function Layout({ children }: React.PropsWithChildren) {
-  return (
-    <div className='border-popup-border flex min-h-[10.5rem] w-[21.25rem] min-w-[21.25rem] flex-col border-[0.5rem] p-3'>
-      {children}
-    </div>
-  );
-}
